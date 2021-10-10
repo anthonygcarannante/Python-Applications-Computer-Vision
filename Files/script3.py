ce3e3be10b7e2cf267f669b37fb65a15f9f78c99
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-10 18:57:53 -0400

    added code for detecting face

--- Files/script3.py
+++ Files/script3.py
@@ -1,11 +1,15 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+# https://stackoverflow.com/questions/30508922/error-215-empty-in-function-detectmultiscale
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-img = cv2.imread("photo.jpg")
+img = cv2.imread("/Users/anthonycarannante/Desktop/Github_Repos/Python-Applications-Computer-Vision/Files/photo.jpg")
 
-# gray_img = cv2.cvtColor(img.cv2.COLOR_BGR2GRAY)
-gray_img = cv2.imread("/Users/anthonycarannante/Desktop/Github_Repos/Python-Applications-Computer-Vision/Files/photo.jpg", 0)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+face = face_cascade.detectMultiScale(gray_img, scaleFactor = 1.05)
+
+print(type(face))
 
 cv2.imshow("Gray",gray_img)
 cv2.waitKey(0)
