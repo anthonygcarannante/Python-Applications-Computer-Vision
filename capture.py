ecf17fb40ffcd5a61054c000f80eaf0c60738910
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-10 20:44:03 -0400

    Improved script for capturing frames in a video from camera

--- capture.py
+++ capture.py
@@ -2,15 +2,26 @@ import cv2, time
 
 video = cv2.VideoCapture(0)
 
+# Iterator value for number of frames captured
+a = 1
+
 while True:
+    a = a+1
     check, frame = video.read()
 
     print(check)
     print(frame)
 
-    time.sleep(3)
-    cv2.imshow("Capturing", frame)
-    cv2.waitKey(0)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Capturing", gray)
 
+    key=cv2.waitKey(1)
+    
+    # Capture video. If captured,
+    if key == ord('q'):
+        break
+
+# Print number of frames captured
+print(a)
 video.release()
 cv2.destroyAllWindows()
