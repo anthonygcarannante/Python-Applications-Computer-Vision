83979c9293551826499efa37f64a9512e40cbb01
carannantea <79670978+carannantea@users.noreply.github.com>
2021-10-17 17:31:46 -0400

    Added tooltip to motion detection data points

--- Motion_Detector/plotting.py
+++ Motion_Detector/plotting.py
@@ -1,14 +1,19 @@
 # Run motiondetector.py and import dataframe of motion detection data
 from motiondetector import df
-
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+
+cds=ColumnDataSource(df)
 
 # Create figure object
-p=figure(x_axis_type='datetime', height=300, width=500, title='Motion Graph')
+p=figure(x_axis_type='datetime', height=200, width=500, title='Motion Graph')
 p.yaxis.minor_tick_line_color=None
 
+hover=HoverTool(tooltips=[('Start', '@Start'),('End', '@End')])
+p.add_tools(hover)
+
 # Create quadrants for plot
-q = p.quad(left=df['Start'],right=df['End'],bottom=0,top=1,color='green')
+q = p.quad(left='Start',right='End',bottom=0,top=1,color='green',source=cds)
 
 output_file("Graph.html")
 show(p)
